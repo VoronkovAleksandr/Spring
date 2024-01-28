4bc582c58161c2a9fe803ccb415338a59e580db4
VoronkovAleksandr <a_voron@list.ru>
2024-01-28 19:51:04 +0500

    HomeWork002 initial.

---
+++ HomeWork002/src/test/java/ru/voronkov/HomeWork002/HomeWork002ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.voronkov.HomeWork002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork002ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
