f936ebd876b493451b46fd60dbbb7b04895cee62
VoronkovAleksandr <a_voron@list.ru>
2024-02-11 13:23:27 +0500

    HomeWork006 completed

--- HomeWork005/src/main/java/ru/voronkov/HomeWork005/controllers/TaskController.java
+++ HomeWork005/src/main/java/ru/voronkov/HomeWork005/controllers/TaskController.java
@@ -14,26 +14,32 @@ public class TaskController {
 
     private final TaskService service;
 
+    //Получаем все задачи
     @GetMapping
     public List<Task> getAllTask(){
         return service.getAllTasks();
     }
 
+
+    //Добавляем новую задачу
     @PostMapping("/add")
     public Task addTask(@RequestBody Task task){
         return service.createTask(task);
     }
 
+    //Получаем задачи со запрошенному статусу
     @GetMapping("/status/{status}")
     public List<Task> getTasksByStatus(@PathVariable Task.TaskStatus status){
         return service.getTaskByStatus(status);
     }
 
+    //Обновляем задачу в БД
     @PutMapping("/{id}")
     public Task updateTaskStatus(@PathVariable Long id, @RequestBody Task task){
         return service.updateTask(id, task);
     }
 
+    //Удаляем задачу
     @DeleteMapping("/{id}")
     public void deleteTask(@PathVariable Long id){
         service.deleteTask(id);
---
+++ HomeWork006/src/main/java/ru/voronkov/HomeWork006/HomeWork006Application.java
@@ -0,0 +1,13 @@
+package ru.voronkov.HomeWork006;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork006Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork006Application.class, args);
+	}
+
+}
