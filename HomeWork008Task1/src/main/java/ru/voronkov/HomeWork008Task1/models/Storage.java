71d4af1aeda596e0d67291bfa7fc15bbf4d545e4
VoronkovAleksandr <a_voron@list.ru>
2024-02-24 23:29:28 +0500

    HomeWork008Task1 completed

---
+++ HomeWork008Task1/src/main/java/ru/voronkov/HomeWork008Task1/models/Storage.java
@@ -0,0 +1,28 @@
+package ru.voronkov.HomeWork008Task1.models;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+
+@Data
+@Entity
+@Table(schema = "storage")
+public class Storage {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @OneToOne
+    @JoinColumn(name= "fk_product_id", referencedColumnName = "product_id")
+    private Product product;
+
+    @Column(nullable = false)
+    private float quantity;
+
+}
+
+
